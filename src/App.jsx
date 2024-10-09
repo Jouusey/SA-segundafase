dd09596f185a5a6488faa912a75ff3003fe074d8
jaime-ac <jaimeantonioca97@gmail.com>
2024-10-09 17:00:13 -0300

    criação e add da tela de cadastro na pasta pages e definição das paletas de cores no arquivo index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import TelaCadastro from './pages/TelaCadastro'
 
 function App() {
 
   return (
     <>
+
+      <div className='app-container'>
+
+        <TelaCadastro />
+
+      </div>
 
     </>
   )
---
+++ src/pages/TelaCadastro.jsx
@@ -0,0 +1,74 @@
+import "./TelaCadastro.css"
+
+export default function TelaCadastro() {
+  return (
+    <div className="container-tela-cadastro">
+
+        <div className="container-conteudo-cadastro">
+
+            <div className="parte-formulario">
+
+                <div className="formulario-cadastro">
+
+                   <h1 className="label-titulo">Cadastro</h1>
+
+                   <div className="classe-inputs">
+
+                        <label htmlFor="label-nome" className="label-inputs">Nome</label>
+                        <input type="text" id="label-nome" className="inputs-cadastro" placeholder="digite seu nome completo"/>
+
+                   </div>
+
+                   <div className="classe-inputs">
+
+                        <label htmlFor="label-usuario" className="label-inputs">Usuário</label>
+                        <input type="text" id="label-usuario" className="inputs-cadastro" placeholder="digite seu nome de usuário"/>
+
+                   </div>
+
+                   <div className="classe-inputs">
+
+                        <label htmlFor="label-email" className="label-inputs">Email</label>
+                        <input type="email" id="label-email" className="inputs-cadastro" placeholder="digite seu endereço de email"/>
+
+                   </div>
+
+                   <div className="classe-inputs">
+
+                        <label htmlFor="label-senha" className="label-inputs">Senha</label>
+                        <input type="password" id="label-senha" className="inputs-cadastro" placeholder="digite uma senha"/>
+
+                   </div>
+
+                </div>
+
+            </div>
+
+            <div className="parte-imagem">
+
+                <div className="parte-grafico">
+
+                    <img className="imagem-parte-cadastro" src="./images/imagem-grafico.png" alt="" />
+
+                </div>
+
+                <div className="classe-botao-cadastro">
+
+                        <button className="botao-cadastro">Cadastrar</button>
+
+                </div>
+
+                <div className="recuperar-conta">
+
+                    <p className="label-possuir-conta">Já possui uma conta?</p>
+
+                </div>
+                
+
+            </div>
+
+        </div>
+      
+    </div>
+  )
+}
