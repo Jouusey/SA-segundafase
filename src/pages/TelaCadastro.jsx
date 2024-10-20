ba4a59ee7094825d46d7c7f001de3475e9e88f4c
Jouusey <josevitor110m.p@gmail.com>
2024-10-19 23:26:43 -0300

    atualização

--- src/pages/TelaCadastro.jsx
+++ src/pages/TelaCadastro.jsx
@@ -12,87 +12,86 @@ export default function TelaCadastro() {
   return (
     <div className="container-tela-cadastro">
  
-        <div className="livro-cadastro-container">
+        <div className="livro-cadastro-container-esquerda">
 
-
-            <div className="livro-cadastro-primeiraLayer">
-
-
-                <div className="livro-cadastro-segundaLayer">
-
-
-                    <div className="livro-cadastro-terceiraLayer">
-
-
-
-                            <div className="livro-cadastro-ConteudoLayer">
+            <div className="livro-cadastro-primeiraLayer-esquerda">
 
 
 
                                 <div className="livro-cadastro-conteudoLayerEsquerda">
                                 
 
+    
+                                <label htmlFor="label-titulo" 
+                                    className="label-titulos">CADASTRO</label>
 
                                     <label htmlFor="label-nome" 
-                                    className="label-inputs">Nome</label>
+                                    className="label-inputs-nome">Nome completo:</label>
 
                                     <input type="text" 
                                     id="label-nome" 
                                     className="inputs-cadastro" 
-                                    placeholder="digite seu nome completo"
+                                    placeholder="Digite seu nome completo"
                                     onChange={(event) => setInputNomeCompleto(event.target.value)}/>
 
                                     <label htmlFor="label-usuario" 
-                                    className="label-inputs">Usuário</label>
+                                    className="label-inputs">Usuario:</label>
 
                                     <input type="text" 
                                     id="label-usuario" 
                                     className="inputs-cadastro" 
-                                    placeholder="digite seu nome de usuário"
+                                    placeholder="Digite seu nome de usuario"
                                     onChange={(event) => setInputNomeUsuario(event.target.value)}/>
 
                                     <label htmlFor="label-email" 
-                                    className="label-inputs">Email</label>
+                                    className="label-inputs">Email:</label>
 
                                     <input type="email" 
                                     id="label-email" 
                                     className="inputs-cadastro" 
-                                    placeholder="digite seu endereço de email"/>
+                                    placeholder="Digite seu endereco de email"/>
                                         
                                     <label htmlFor="label-senha" 
-                                    className="label-inputs">Senha</label>
+                                    className="label-inputs">Senha:</label>
 
                                     <input type="password" 
                                     id="label-senha" 
                                     className="inputs-cadastro" 
-                                    placeholder="digite uma senha"/>
-
-                                    {inputNomeCompleto}
-                                    {inputNomeUsuario}
-                                    {inputEmail}
-                                    {inputSenha}
+                                    placeholder="Digite uma senha"/>
 
                                 </div>
 
-                                <div className="livro-cadastro-conteudoLayerDireita">
-
-
-
-                                {/* <img className="imagem-parte-cadastro" src="./images/imagem-grafico.png" alt="" /> */}
-
-                                <button className="botao-cadastro">Cadastrar</button>
-
-                                <p className="label-possuir-conta">Já possui uma conta?</p>
-                                
-
-
-
-                                </div>
-                            </div>
-                    </div>
-                </div>
+                          
+                    
+                
             </div>
         </div>
+
+        
+
+        <div className="livro-cadastro-container-direita">
+
+
+        <div className="livro-cadastro-primeiraLayerDireita">
+
+                    <div className="livro-cadastro-conteudoLayerDireita">
+
+            <img className="imagem-parte-cadastro" src="./images/imagem-grafico.png" alt="" /> 
+
+            <button className="botao-cadastro">Cadastrar</button>
+
+            <p className="label-possuir-conta">Ja possui uma conta?</p>
+
+
+                    </div>
+
+
+</div>
+
+
+            
+        </div>
     </div>
+
   )
 }
