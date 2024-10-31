da7145282153ac8364ee4190026324c9d6147483
jaime-ac <jaimeantonioca97@gmail.com>
2024-10-31 17:20:06 -0300

    Update da tela de livro

---
+++ src/components/LivroParteDois.jsx
@@ -0,0 +1,67 @@
+import "./LivroParteDois.css"
+
+function LivroParteDois() {
+  return (
+    <div className="container-mae-resenhas">
+
+        <div className="container-resenhas">
+
+            <div className="container-resenha-usuarios">
+
+                <div className="box-resenha">
+
+                    <div className="parte-foto-nome">
+
+                        <div className="foto-perfil">
+
+                        </div>
+
+                        <h3>nome do perfil</h3>
+
+                    </div>
+
+                    <div className="parte-resenha">
+
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta reprehenderit dignissimos, minus, earum ad laudantium facilis error, fugit quibusdam at nulla fugiat molestiae labore omnis perspiciatis veniam illum aliquid non?</p>
+
+                    </div>
+
+                    <div className="parte-curtida">
+
+                    </div>
+
+                </div>
+
+                <div className="box-resenha">
+
+                    <div className="parte-foto-nome">
+
+                        <div className="foto-perfil">
+
+                        </div>
+
+                        <h3>nome do perfil</h3>
+
+                    </div>
+
+                    <div className="parte-resenha">
+
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta reprehenderit dignissimos, minus, earum ad laudantium facilis error, fugit quibusdam at nulla fugiat molestiae labore omnis perspiciatis veniam illum aliquid non?</p>
+
+                    </div>
+
+                    <div className="parte-curtida">
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default LivroParteDois
--- src/pages/TelaLivro.jsx
+++ src/pages/TelaLivro.jsx
@@ -1,3 +1,4 @@
+import LivroParteUm from "../components/LivroParteUm"
 import "./TelaLivro.css"
 
 function TelaLivro() {
@@ -5,75 +6,8 @@ function TelaLivro() {
 
     <div className="container-mae">
       
-
-      <div className="container-tela">
-
-        <div className="parte-cima">
-
-          <div className="parte-capa-livro">
-
-            <div className="capa-livro"></div>
-
-          </div>
-
-          <div className="parte-textos">
-
-            <div className="textos">
-
-              <div className="titulo-livro">
-
-                <h3>Título do livro</h3>
-
-              </div>
-
-              <div className="descricao-livro">
-
-                <h4>#autor</h4>
-                <h4>#editora</h4>
-                <h4>#ano</h4>
-                <h4>#volume</h4>
-
-              </div>
-
-              <div className="sinopse-livro">
-
-                <h3>Sinopse:</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nulla sapiente magni, ea non voluptates est dolorum vero aperiam necessitatibus tempora consectetur animi modi provident amet, deleniti qui. Quidem, corrupti.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nulla sapiente magni, ea non voluptates est dolorum vero aperiam necessitatibus tempora consectetur animi modi provident amet, deleniti qui. Quidem, corrupti.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nulla sapiente magni, ea non voluptates est dolorum vero aperiam necessitatibus tempora consectetur animi modi provident amet, deleniti qui. Quidem, corrupti.</p>
-
-              </div>
-
-              <div className="genero-livro">
-
-                <div className="box-genero">#genero1</div>
-                <div className="box-genero">#genero2</div>
-                <div className="box-genero">#genero3</div>
-
-              </div>
-
-            </div>
-
-          </div>
-
-          <div className="parte-menus">
-
-            <button className="botao-menu"><img src="./images/home.svg" alt="" className="icone-botao"/></button>
-            <button className="botao-menu"><img src="./images/setting.svg" alt="" className="icone-botao"/></button>
-            <button className="botao-menu"><img src="./images/pen.svg" alt="" className="icone-botao"/></button>
-
-          </div>
-
-        </div>
-
-        <div className="parte-baixo">
-
-          <button className="botao-resenha">Resenhas  |  02</button>
-          <button className="botao-icone"><img src="./images/down.svg" alt="" className="icone-down"/></button>
-
-        </div>
-
-      </div>
+      <LivroParteUm />
+      
       
     </div>
 
