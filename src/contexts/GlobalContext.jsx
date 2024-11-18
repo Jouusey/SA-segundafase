5cb0714d9754d57da6a07e64bb59fa13b489560e
cassianobigodudo <kakacassiano2010@hotmail.com>
2024-11-18 17:04:04 -0300

    lógica de usuariologado no cadastro (falta a do login)

--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -8,7 +8,7 @@ export const GlobalContextProvider = ({children}) => {
 const [bairro, setBairro] = useState('Monte Verde')
 const [vetorObjetosUsuarios, setVetorObjetosUsuarios] = useState([])
 const [usuarioLogado, setUsuarioLogado] = useState(false)
-const [posicaoUsuario, setPosicaoUsuario] = useState()
+const [posicaoUsuario, setPosicaoUsuario] = useState('vazio')
 
     return(
         <GlobalContext.Provider value={{bairro, setBairro, vetorObjetosUsuarios, setVetorObjetosUsuarios, usuarioLogado, setUsuarioLogado, posicaoUsuario, setPosicaoUsuario}}>
--- src/pages/TelaUsuarioConfigs.jsx
+++ src/pages/TelaUsuarioConfigs.jsx
@@ -5,16 +5,28 @@ import{ GlobalContext } from '../contexts/GlobalContext'
 import { useContext } from 'react'
 import IconUserCircle from "../components/IconUserCircle"
 import ResenhasConfigs from "../components/ResenhasConfigs"
+import Navbar from "../components/Navbar"
 
 function TelaUsuarioConfigs() {
+
+  const {posicaoUsuario, setPosicaoUsuario, vetorObjetosUsuarios, setVetorObjetosUsuarios, usuarioLogado, setUsuarioLogado}=useContext(GlobalContext)
+
+
+
+
   return (
     <div className="usuarioConfigs-container">
 
       <div className="usuarioConfigs-div-esquerda">
 
+
+
       </div>
 
       <div className="usuarioConfigs-body">
+        
+        <Navbar/>
+        
 
         <div className="usuarioConfigs-body-cima">
 
@@ -30,15 +42,15 @@ function TelaUsuarioConfigs() {
 
                 <label className="lbl-titulos">Configurações de usuário</label>
                 <IconUserCircle/>
-                <label className="lbl-nome-usuario" >Nome de Usuário</label>
+                <label className="lbl-nome-usuario" >{vetorObjetosUsuarios[posicaoUsuario].nome}</label>
 
               </div>
 
               <div className="usuarioConfigs-bmpc-infos">
 
-              <label className="lbl-infos" >Nome Completo</label>
-              <label className="lbl-infos" >Email</label>
-              <label className="lbl-infos" >Senha</label>
+              <label className="lbl-infos" >{vetorObjetosUsuarios[posicaoUsuario].usuario}</label>
+              <label className="lbl-infos" >{vetorObjetosUsuarios[posicaoUsuario].email}</label>
+              <label className="lbl-infos" >{vetorObjetosUsuarios[posicaoUsuario].senha}</label>
 
                 <input type="text" className="input" placeholder="Edite seu nome completo" />
                 <input type="text" className="input" placeholder="Edite seu email" />
@@ -52,6 +64,7 @@ function TelaUsuarioConfigs() {
               <div className="usuarioConfigs-bmpc-buttons">
 
                 <button className="btn">Editar dados</button>
+                <button className="btn" >Deslogar</button>
                 <button className="btn btn-delete">Apagar conta</button>
               </div>
 
